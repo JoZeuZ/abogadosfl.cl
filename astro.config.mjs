24c3a1dbfc382b2c76da78559b61ffb8d5ede3f8
JoZeuZz <joserodriguez.civilinformatico@gmail.com>
2025-10-27 20:57:28 -0300

    Guardado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { astroImageTools } from "astro-imagetools";
 
 export default defineConfig({
   site: 'https://www.abogadosfl.cl', // URL base del sitio
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroImageTools],
   output: 'static',
   build: {
     assets: 'assets'
